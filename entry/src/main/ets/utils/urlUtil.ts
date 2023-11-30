f121688ff4337abbdf045aec1a7400d90c110376
wangtao <wangtao.cpu@gmail.com>
2023-12-01 00:39:34 +0800

    修复地址栏

--- entry/src/main/ets/utils/urlUtil.ts
+++ entry/src/main/ets/utils/urlUtil.ts
@@ -1,4 +1,17 @@
-export  const isUrlString = str => {
+export const isUrlString = str => {
   var pattern = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\{0}'\*\+,;=.]+$/
   return pattern.test(str)
+}
+
+export const  searchAPI = (engine: string, keyword: string) => {
+  switch (engine) {
+    case 'bing':
+      return `https://bing.com/search?q=${keyword}`
+    case 'baidu':
+      return `https://www.baidu.com/s?wd=${keyword}`
+    case 'google':
+      return `https://www.google.com/search?q=${keyword}`
+    case 'sogou':
+      return `https://www.sogou.com/web?query=${keyword}`
+  }
 }
